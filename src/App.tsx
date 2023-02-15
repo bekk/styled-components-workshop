7fda8395fb9ee6bf47f97f16e15bb8d292c0e85b
annelinegjersem <annelinegj@gmail.com>
2023-02-15 16:32:18 +0100

    Legge til en Plante Co-authored-by: Ingrid Fosså <ingrid.fossa@bekk.no>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Plant } from "./Components/Plant";
 
 function App() {
     return (
         <div className="App">
-           <header>
-               <h1>Alt om planter</h1>
-           </header>
+            <header>
+                <h1>Alt om planter</h1>
+            </header>
+            <Plant />
         </div>
     );
 }
---
+++ src/Components/Plant.tsx
@@ -0,0 +1,12 @@
+export const Plant = () => {
+    return (
+        <a href="#">
+            <div>Bilde</div>
+            <h2>Monstera</h2>
+            <p>
+                Denne planten er også kjent som vindusblad, men visste du at
+                frukten er spiselig?...
+            </p>
+        </a>
+    );
+};
