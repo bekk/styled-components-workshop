d2ad4e1fa905cb8776a7474445ae0c7a2ef3d835
annelinegjersem <annelinegj@gmail.com>
2023-03-16 15:35:19 +0100

    added URL for images

--- src/Data/Plants.tsx
+++ src/Data/Plants.tsx
@@ -10,6 +10,8 @@ export const plants = [
             "Place in bright, indirect sunlight",
             "Keep in a warm, humid environment",
         ],
+        imageUrl:
+            "https://www.mestergronn.no/dw/image/v2/BFMT_PRD/on/demandware.static/-/Sites-product-catalog/no_NO/v1678952724901//mg-product-images/158437/158437_1.jpg?sw=800&sfrm=jpg&q=80",
     },
     {
         id: 2,
@@ -22,6 +24,8 @@ export const plants = [
             "Can tolerate low light conditions",
             "Prefers well-draining soil",
         ],
+        imageUrl:
+            "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F34%2F2022%2F09%2F21%2Feight-houseplants-that-thrive-in-low-light-8-0922-2000.jpg",
     },
     {
         id: 3,
@@ -34,6 +38,8 @@ export const plants = [
             "Place in bright, indirect sunlight",
             "Avoid cold drafts and temperature fluctuations",
         ],
+        imageUrl:
+            "https://bloomscape.com/wp-content/uploads/2020/08/bloomscape_fiddle-leaf-fig_charcoal-e1652800894846.jpg",
     },
     {
         id: 4,
@@ -46,7 +52,10 @@ export const plants = [
             "Can tolerate low to bright, indirect sunlight",
             "Prefers well-draining soil",
         ],
+        imageUrl:
+            "https://houseplantresourcecenter.com/wp-content/uploads/2020/09/severin-candrian-IZz3sboy1g8-unsplash-1.jpg",
     },
+
     {
         id: 5,
         name: "Spider Plant",
@@ -58,6 +67,8 @@ export const plants = [
             "Prefers bright, indirect sunlight",
             "Thrives in humid environments",
         ],
+        imageUrl:
+            "https://www.bhg.com/thmb/LrCakiEVtRCyslgobTNlo-suVMk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/BHG-spider-plant-c0e0fdd5ec6e4c1588998ce3167f6579.jpg",
     },
     {
         id: 6,
@@ -70,6 +81,7 @@ export const plants = [
             "Can tolerate low to bright, indirect sunlight",
             "Prefers well-draining soil",
         ],
+        imageUrl:
+            "https://assets.eflorist.com/site/32084100/assets/products/PZM_/sku4760119.png?1671473999666",
     },
-    // Add more plants as needed...
 ];
