48f3ff69d8606fb16f2efd0cb5469c65a0e56340
annelinegjersem <annelinegj@gmail.com>
2023-03-16 15:21:06 +0100

    Plants data

---
+++ src/Data/Plants.tsx
@@ -0,0 +1,75 @@
+export const plants = [
+    {
+        id: 1,
+        name: "Monstera Deliciosa",
+        image: require("./images/monstera.jpg"),
+        description:
+            "The Monstera Deliciosa is a popular houseplant known for its large, glossy leaves. It is native to the rainforests of Central and South America.",
+        careInstructions: [
+            "Water once a week",
+            "Place in bright, indirect sunlight",
+            "Keep in a warm, humid environment",
+        ],
+    },
+    {
+        id: 2,
+        name: "Snake Plant",
+        image: require("./images/snake_plant.jpg"),
+        description:
+            "The Snake Plant, also known as Sansevieria, is a hardy and low-maintenance plant that can thrive in a variety of conditions. It is native to West Africa.",
+        careInstructions: [
+            "Water sparingly, only when soil is dry",
+            "Can tolerate low light conditions",
+            "Prefers well-draining soil",
+        ],
+    },
+    {
+        id: 3,
+        name: "Fiddle Leaf Fig",
+        image: require("./images/fiddle_leaf_fig.jpg"),
+        description:
+            "The Fiddle Leaf Fig is a popular houseplant known for its large, violin-shaped leaves. It is native to tropical regions of Africa.",
+        careInstructions: [
+            "Water once a week, allowing soil to dry slightly between waterings",
+            "Place in bright, indirect sunlight",
+            "Avoid cold drafts and temperature fluctuations",
+        ],
+    },
+    {
+        id: 4,
+        name: "Pothos",
+        image: require("./images/pothos.jpg"),
+        description:
+            "The Pothos is a trailing vine plant that is easy to care for and can grow well in a variety of lighting conditions. It is native to the Solomon Islands.",
+        careInstructions: [
+            "Water when the top inch of soil feels dry",
+            "Can tolerate low to bright, indirect sunlight",
+            "Prefers well-draining soil",
+        ],
+    },
+    {
+        id: 5,
+        name: "Spider Plant",
+        image: require("./images/spider_plant.jpg"),
+        description:
+            "The Spider Plant is a popular houseplant known for its long, thin leaves and small white flowers. It is native to tropical and southern Africa.",
+        careInstructions: [
+            "Water once a week, allowing soil to dry slightly between waterings",
+            "Prefers bright, indirect sunlight",
+            "Thrives in humid environments",
+        ],
+    },
+    {
+        id: 6,
+        name: "ZZ Plant",
+        image: require("./images/zz_plant.jpg"),
+        description:
+            "The ZZ Plant is a low-maintenance houseplant that can grow well in low light conditions. It is native to Eastern Africa.",
+        careInstructions: [
+            "Water sparingly, only when soil is dry",
+            "Can tolerate low to bright, indirect sunlight",
+            "Prefers well-draining soil",
+        ],
+    },
+    // Add more plants as needed...
+];
