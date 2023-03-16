b4602138eaad6417602486cfa32a1594be09f9cc
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-16 15:32:54 +0100

    Add const for styling App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Oppgaver } from "./Oppgaver";
+import styled from "styled-components";
+
+const Container = styled.div`
+  // TODO add base styling here
+
+`;
 
 function App() {
     return (
