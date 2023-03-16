a4fa0b1ab349d6c9ec4f0159047882c36f0a2092
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-16 15:52:03 +0100

    Display All The Plants! :D
    Co-authored-by: Anne Line Gjersem <anneline.gjersem@bekk.no>

--- src/Oppgaver.tsx
+++ src/Oppgaver.tsx
@@ -3,13 +3,14 @@ import { Plant } from "./Components/Plant";
 import { plants } from "./Data/Plants";
 
 export const Oppgaver = () => {
-
     return (
         <>
             <header>
                 <h1>Plant World</h1>
             </header>
-            <Plant plant={plants[0]}/>
+            {
+                plants.map(plant => <Plant plant={plant} />)
+            }
         </>
     )
 }
