257e8ab82afadf0c79e0d74fc77e553e3e45396e
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-17 10:08:39 +0100

    Add enum for shadows

---
+++ src/StyleTokens/shadow.ts
@@ -0,0 +1,7 @@
+export enum SHADOW {
+    XSMALL = "0 1px 2px 0 rgba(0, 0, 0, 0.12)",
+    SMALL = "0 1px 2px -1px rgba(0, 0, 0, 0.1), 0 1px 3px 0 rgba(0, 0, 0, 0.1)",
+    MEDIUM = "0 8px 10px -6px rgba(0, 0, 0, 0.1), 0 25px 50px -5px rgba(0, 0, 0, 0.1)",
+    LARGE = "0 4px 6px -4px rgba(0, 0, 0, 0.1), 0 10px 15px -3px rgba(0, 0, 0, 0.1)",
+    XLARGE = "0 8px 10px -6px rgba(0, 0, 0, 0.1), 0 25px 50px -5px rgba(0, 0, 0, 0.1)"
+}
