e3109a4cf16fdb0608bc4bbf7e5ea8f1bd0c106f
annelinegjersem <annelinegj@gmail.com>
2023-03-17 13:29:16 +0100

    Add Solution 2c

--- src/Fasit/Solution.tsx
+++ src/Fasit/Solution.tsx
@@ -1,8 +1,10 @@
 import { Task1 } from "./Task1";
 import { Task2a } from "./Task2a";
+import { Task2c } from "./Task2c";
 
 
 export const Solution = () => {
     // return <Task1 />;
-     return <Task2a />;
+    // return <Task2a />;
+     return <Task2c />;
 };
---
+++ src/Fasit/Task2c.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { Plants } from "../Components/Plants";
+import { BREAKPOINTS } from "../StyleTokens/breakpoints";
+const Heading = styled.h1`
+    font-size: 2rem;
+    font-weight: bold;
+    text-decoration-line: underline;
+    text-decoration-style: wavy;
+
+    &:hover {
+        text-decoration: underline;
+    }
+    @media (min-width: ${BREAKPOINTS.TABLET}) {
+        font-size: 3rem;
+    }
+`;
+
+export const Task2c = () => {
+    return (
+        <>
+            <header>
+                <Heading>Plant World (Solution)</Heading>
+            </header>
+            <Plants />
+        </>
+    );
+};
