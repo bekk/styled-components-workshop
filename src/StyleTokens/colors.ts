4122fde4d01ba319b357431b1fbb5906ee939cad
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-17 10:33:59 +0100

    Add color constants
    Stolen from https://tailwindcss.com/docs/customizing-colors (Neutral gray + Lime)

---
+++ src/StyleTokens/colors.ts
@@ -0,0 +1,24 @@
+// Colors from https://tailwindcss.com/docs/customizing-colors
+export enum COLORS {
+    GRAY_050 = "#fafafa",
+    GRAY_100 = "#f5f5f5",
+    GRAY_200 = "#e5e5e5",
+    GRAY_300 = "#d4d4d4",
+    GRAY_400 = "#a3a3a3",
+    GRAY_500 = "#737373",
+    GRAY_600 = "#525252",
+    GRAY_700 = "#404040",
+    GRAY_800 = "#262626",
+    GRAY_900 = "#171717",
+
+    LIME_050 = "#f7fee7",
+    LIME_100 = "#ecfccb",
+    LIME_200 = "#d9f99d",
+    LIME_300 = "#bef264",
+    LIME_400 = "#a3e635",
+    LIME_500 = "#84cc16",
+    LIME_600 = "#65a30d",
+    LIME_700 = "#4d7c0f",
+    LIME_800 = "#3f6212",
+    LIME_900 = "#365314",
+}
