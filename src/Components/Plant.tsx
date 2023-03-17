e151858a57491f39d9b7c2a7ecfe52f0b48df396
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-17 10:39:30 +0100

    Add base styling of card

--- src/Components/Plant.tsx
+++ src/Components/Plant.tsx
@@ -1,4 +1,16 @@
+import styled from "styled-components";
 import { Plant as PlantType } from "../Data/Plants"
+import { SHADOW } from "../StyleTokens/shadow";
+import { BORDER_RADIUS } from "../StyleTokens/borderRadius";
+import { COLORS } from "../StyleTokens/colors";
+
+const Card = styled.a`
+  display: block;
+  width: 600px;
+  background: ${COLORS.WHITE};
+  box-shadow: ${SHADOW.SMALL};
+  border-radius: ${BORDER_RADIUS.LARGE};
+`;
 
 interface Props {
     plant: PlantType;
@@ -6,12 +18,12 @@ interface Props {
 
 export const Plant = ({ plant }: Props) => {
     return (
-        <a href="#">
+        <Card href="#">
             <h2>{plant.name}</h2>
-            <div><img src={plant.imageUrl} alt=""/></div>
+            <div><img src={plant.imageUrl} alt="" /></div>
             <p>
                 {plant.description}
             </p>
-        </a>
+        </Card>
     );
 };
